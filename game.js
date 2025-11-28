0d64a0225f19024fe7a07e486f292c25bc3f3c72
Maisie90 <maisiebishop@yahoo.co.uk>
2025-11-28 15:21:43 +0000

    added code to game js and html and css

--- game.js
+++ game.js
@@ -1,43 +1,73 @@
-const readline = require("readline");
-const userInput = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const sidesInput = document.getElementById("sidesInput");
+const startButton = document.getElementById("startButton");
+const guessInput = document.getElementById("guessInput");
+const guessButton = document.getElementById("guessButton");
+const messages = document.getElementById("messages");
+const sidesContainer = document.getElementById("sidesContainer")
+const guessContainer = document.getElementById("guessContainer")
+const playAgainButton = document.getElementById("playAgainButton")
 function rollDice(sides) {
-  const diceResult = Math.floor(Math.random() * sides) + 1;
-  return diceResult;
+  return Math.floor(Math.random() * sides) + 1;
 }
-function startGame(){
-userInput.question("How many sides?: ", sidesInput => {
-    const sides = Number(sidesInput);
-    if ( sides <= 2 || isNaN(sides) || Number.isInteger(sides) === false){
-        console.log("Please pick a integer greater than 2")
-        return startGame();
-    }
-    else{
-    console.log("you picked: " + sides + " number of sides")
-    playGame(sides);
-    }
+startButton.addEventListener("click", () => {
+  const sides = Number(sidesInput.value);
+  if (!sidesInput.value){
+    messages.textContent = "please enter a number of sides"
+    return;
+  }
+  else if (!Number.isInteger(sides)) {
+    messages.textContent = "Pick a integer";
+    return;
+  }
+  else if (sides <=2) {
+    messages.textContent = "Pick a integer greater than 2";
+    return;
+  }
+  sidesContainer.style.display = "none"
+  guessContainer.style.display = "block"
+  messages.textContent = `You have chosen a: ${sides} sided dice!`;
+});
+guessButton.addEventListener("click", () => {
+const sides = Number(sidesInput.value)
+const userGuess = Number(guessInput.value)
+if (!sidesInput.value || !Number.isInteger(sides) || sides <=2){
+    messages.textContent = "Please pick a number of sides FIRST"
+    return;
+}
+if(!guessInput.value){
+    messages.textContent = "Enter Your guess"
+    return;
+}
+if(!Number.isInteger(userGuess)){
+    messages.textContent = "guess must be a integer"
+    return;
+}
+if (userGuess < 1 || userGuess > sides){
+    messages.textContent = "please guess in our range"
+    return;
+}
+const roll = rollDice(sides);
+messages.textContent = `You have rolled a: ${roll}!`
+if (userGuess === roll) {
+    messages.textContent += " You are a winner!"
+    const gif1 = document.createElement('img');
+    gif1.src= `assets/winner.gif`;
+    imageContainer.innerHTML = "";
+    imageContainer.appendChild(gif1)
+} else {messages.textContent +=`, but you guessed ${userGuess} which was wrong.`
+ const gif1 = document.createElement('img');
+    gif1.src= `assets/loser.gif`;
+    imageContainer.innerHTML = "";
+    imageContainer.appendChild(gif1)}
+
+guessContainer.style.display = "none"
+playAgainButton.style.display = "block"
+});
+playAgainButton.addEventListener("click", () => {
+    sidesInput.value ="";
+    guessInput.value ="";
+    messages.textContent = "";
+    sidesContainer.style.display = "block";
+    guessContainer.style.display = "none"
+    playAgainButton.style.display = "none"
 })
-}
-function playGame(sides) {
-  userInput.question("Guess hte die number: ", GuessedInput => {
-    const userGuess = Number(GuessedInput);
-    const roll = rollDice(sides);
-    console.log(`You Rolled a: ${roll}!`);
-    if (userGuess === roll) {
-      console.log("Winner");
-    } else {
-      console.log("wrong");
-    }
-    userInput.question("Play again? yes/no: ", answer => {
-        console.log(answer)
-        if (answer.toLowerCase() === "yes"){
-            startGame();
-        } else {console.log("game terminates")
-            userInput.close();
-        }
-    })
-  });
-}
-startGame();
